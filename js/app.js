c7dd372acd2a802da58b3bb987458c4e527cc0f5
Ivan Kucherov <kucher_up@mail.ru>
2022-09-01 02:08:15 +0300

    11.2

--- js/app.js
+++ js/app.js
@@ -19507,8 +19507,8 @@ PERFORMANCE OF THIS SOFTWARE.
             return new LightGallery(el, options);
         }
         const lightgallery_es5 = lightGallery;
-        __webpack_require__(5097);
-        __webpack_require__(5086);
+        var lg_thumbnail_min = __webpack_require__(5097);
+        var lg_zoom_min = __webpack_require__(5086);
         const galleries = document.querySelectorAll("[data-gallery]");
         if (galleries.length) {
             let galleyItems = [];
@@ -19523,6 +19523,84 @@ PERFORMANCE OF THIS SOFTWARE.
             }));
             flsModules.gallery = galleyItems;
         }
+        const $lgContainer = document.getElementById("inline-gallery-container");
+        const inlineGallery = lightgallery_es5($lgContainer, {
+            container: $lgContainer,
+            dynamic: true,
+            hash: false,
+            closable: false,
+            showMaximizeIcon: true,
+            appendSubHtmlTo: ".lg-item",
+            slideDelay: 400,
+            plugins: [ lg_zoom_min, lg_thumbnail_min ],
+            dynamicEl: [ {
+                src: "@img/2.jpg",
+                responsive: "@img/2.jpg",
+                thumb: "@img/2.jpg",
+                subHtml: `<div class="lightGallery-captions">\n                    <h4>Photo by <a href="https://unsplash.com/@dann">Dan</a></h4>\n                    <p>Published on November 13, 2018</p>\n                </div>`
+            }, {
+                src: "https://images.unsplash.com/photo-1473876988266-ca0860a443b8?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1400&q=80",
+                responsive: "https://images.unsplash.com/photo-1473876988266-ca0860a443b8?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=480&q=80 480, https://images.unsplash.com/photo-1473876988266-ca0860a443b8?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80 800",
+                thumb: "https://images.unsplash.com/photo-1473876988266-ca0860a443b8?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=240&q=80",
+                subHtml: `<div class="lightGallery-captions">\n                    <h4>Photo by <a href="https://unsplash.com/@kylepyt">Kyle Peyton</a></h4>\n                    <p>Published on September 14, 2016</p>\n                </div>`
+            }, {
+                src: "https://images.unsplash.com/photo-1588953936179-d2a4734c5490?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1400&q=80",
+                responsive: "https://images.unsplash.com/photo-1588953936179-d2a4734c5490?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=480&q=80 480, https://images.unsplash.com/photo-1588953936179-d2a4734c5490?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=800&q=80 800",
+                thumb: "https://images.unsplash.com/photo-1588953936179-d2a4734c5490?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=240&q=80",
+                subHtml: `<div class="lightGallery-captions">\n                    <h4>Photo by <a href="https://unsplash.com/@jxnsartstudio">Garrett Jackson</a></h4>\n                    <p>Published on May 8, 2020</p>\n                </div>`
+            }, {
+                src: "https://images.unsplash.com/photo-1591634616938-1dfa7ee2e617?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1400&q=80",
+                responsive: "https://images.unsplash.com/photo-1591634616938-1dfa7ee2e617?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=480&q=80 480, https://images.unsplash.com/photo-1591634616938-1dfa7ee2e617?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80 800",
+                thumb: "https://images.unsplash.com/photo-1591634616938-1dfa7ee2e617?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=240&q=80",
+                subHtml: `<div class="lightGallery-captions">\n                    <h4>Photo by <a href="https://unsplash.com/@brookecagle">Brooke Cagle</a></h4>\n                    <p>Description of the slide 4</p>\n                </div>`
+            }, {
+                src: "https://images.unsplash.com/photo-1543059509-6d53dbee1728?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1400&q=80",
+                responsive: "https://images.unsplash.com/photo-1543059509-6d53dbee1728?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=480&q=80 480, https://images.unsplash.com/photo-1543059509-6d53dbee1728?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=800&q=80 800",
+                thumb: "https://images.unsplash.com/photo-1543059509-6d53dbee1728?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=240&q=80",
+                subHtml: `<div class="lightGallery-captions">\n                    <h4>Photo by <a href="https://unsplash.com/@charlespostiaux">Charles Postiaux</a></h4>\n                    <p>Published on November 24, 2018</p>\n                </div>`
+            }, {
+                src: "https://images.unsplash.com/photo-1609902726285-00668009f004?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1400&q=80",
+                responsive: "https://images.unsplash.com/photo-1609902726285-00668009f004?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=480&q=80 480, https://images.unsplash.com/photo-1609902726285-00668009f004?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=800&q=80 800",
+                thumb: "https://images.unsplash.com/photo-1609902726285-00668009f004?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=240&q=80",
+                subHtml: `<div class="lightGallery-captions">\n                    <h4>Photo by <a href="https://unsplash.com/@bruno_adam">Bruno Adam</a></h4>\n                    <p>Published on January 6, 2021</p>\n                </div>`
+            }, {
+                src: "https://images.unsplash.com/photo-1573007974656-b958089e9f7b?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1400&q=80",
+                responsive: "https://images.unsplash.com/photo-1573007974656-b958089e9f7b?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=480&q=80 480, https://images.unsplash.com/photo-1573007974656-b958089e9f7b?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80 800",
+                thumb: "https://images.unsplash.com/photo-1573007974656-b958089e9f7b?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=240&q=80",
+                subHtml: `<div class="lightGallery-captions">\n                    <h4>Photo by <a href="https://unsplash.com/@sigmund">Sigmund</a></h4>\n                    <p>Published on November 6, 2019</p>\n                </div>`
+            }, {
+                src: "https://images.unsplash.com/photo-1579406842270-ea87c39a8a12?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1400&q=80",
+                responsive: "https://images.unsplash.com/photo-1579406842270-ea87c39a8a12?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=480&q=80 480, https://images.unsplash.com/photo-1579406842270-ea87c39a8a12?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80 800",
+                thumb: "https://images.unsplash.com/photo-1579406842270-ea87c39a8a12?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=240&q=80",
+                subHtml: `<div class="lightGallery-captions">\n                    <h4>Photo by <a href="https://unsplash.com/@chow_parij">Parij Borgohain</a></h4>\n                    <p>Published on January 19, 2020</p>\n                </div>`
+            }, {
+                src: "https://images.unsplash.com/photo-1598911543663-37d77962beb1?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1400&q=80",
+                responsive: "https://images.unsplash.com/photo-1598911543663-37d77962beb1?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=480&q=80 480, https://images.unsplash.com/photo-1598911543663-37d77962beb1?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=800&q=80 800",
+                thumb: "https://images.unsplash.com/photo-1598911543663-37d77962beb1?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=240&q=80",
+                subHtml: `<div class="lightGallery-captions">\n                    <h4>Photo by <a href="https://unsplash.com/@inespiazzese">Ines Piazzese</a></h4>\n                    <p>Published on September 1, 2020</p>\n                </div>`
+            }, {
+                src: "https://images.unsplash.com/photo-1560885673-2cdc12600ec8?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1400&q=80",
+                responsive: "https://images.unsplash.com/photo-1560885673-2cdc12600ec8?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=480&q=80 480, https://images.unsplash.com/photo-1560885673-2cdc12600ec8?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=800&q=80 800",
+                thumb: "https://images.unsplash.com/photo-1560885673-2cdc12600ec8?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=240&q=80",
+                subHtml: `<div class="lightGallery-captions">\n                    <h4>Photo by <a href="https://unsplash.com/@rdsaunders">Richard Saunders</a></h4>\n                    <p>Published on June 19, 2019</p>\n                </div>`
+            }, {
+                src: "https://images.unsplash.com/photo-1571292064306-669f0e758231?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1400&q=80",
+                responsive: "https://images.unsplash.com/photo-1571292064306-669f0e758231?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=480&q=80 480, https://images.unsplash.com/photo-1571292064306-669f0e758231?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80 800",
+                thumb: "https://images.unsplash.com/photo-1571292064306-669f0e758231?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=240&q=80",
+                subHtml: `<div class="lightGallery-captions">\n                    <h4>Photo by <a href="https://unsplash.com/@jalanmeier">J. Meier</a></h4>\n                    <p>Published on October 17, 2019</p>\n                </div>`
+            }, {
+                src: "https://images.unsplash.com/photo-1601935111741-ae98b2b230b0?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1400&q=80",
+                responsive: "https://images.unsplash.com/photo-1601935111741-ae98b2b230b0?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=480&q=80 480, https://images.unsplash.com/photo-1601935111741-ae98b2b230b0?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=800&q=80 800",
+                thumb: "https://images.unsplash.com/photo-1601935111741-ae98b2b230b0?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=240&q=80",
+                subHtml: `<div class="lightGallery-captions">\n                    <h4>Photo by <a href="https://unsplash.com/@brookecagle">Brooke Cagle</a></h4>\n                    <p>Published on October 6, 2020</p>\n                </div>`
+            } ],
+            thumbWidth: 60,
+            thumbHeight: "40px",
+            thumbMargin: 4
+        });
+        setTimeout((() => {
+            inlineGallery.openGallery();
+        }), 200);
         function DynamicAdapt(type) {
             this.type = type;
         }
@@ -20075,72 +20153,45 @@ PERFORMANCE OF THIS SOFTWARE.
             labels: "% 2-х очковых",
             data: [ 55.6, 44.4 ],
             borderColor: "rgba(255, 255, 255, 1)",
-            borderWidth: 2,
-            backgroundColor: [ "rgb(255, 205, 86)", "#ffffff" ],
-            hoverOffset: 5
+            borderWidth: 1,
+            backgroundColor: [ "#fec42f", "#ffffff" ]
         };
         if ($jsChart1) new Chart($jsChart1, {
             type: "doughnut",
             data: {
                 datasets: [ jsChart1Y ]
             },
-            options: {
-                scales: {
-                    yAxes: [ {
-                        ticks: {
-                            beginAtZero: true
-                        }
-                    } ]
-                }
-            }
+            options: {}
         });
         const $jsChart2 = document.querySelector("#jsChart2");
         const jsChart2Y = {
             labels: "% 3-х очковых",
             data: [ 37.9, 62.1 ],
             borderColor: "rgba(255, 255, 255, 1)",
-            borderWidth: 2,
-            backgroundColor: [ "rgb(255, 205, 86)", "#ffffff" ],
-            hoverOffset: 5
+            borderWidth: 1,
+            backgroundColor: [ "#fec42f", "#ffffff" ]
         };
         if ($jsChart2) new Chart($jsChart2, {
             type: "doughnut",
             data: {
                 datasets: [ jsChart2Y ]
             },
-            options: {
-                scales: {
-                    yAxes: [ {
-                        ticks: {
-                            beginAtZero: true
-                        }
-                    } ]
-                }
-            }
+            options: {}
         });
         const $jsChart3 = document.querySelector("#jsChart3");
         const jsChart3Y = {
             labels: "% штрафных",
             data: [ 79.5, 21.5 ],
             borderColor: "rgba(255, 255, 255, 1)",
-            borderWidth: 2,
-            backgroundColor: [ "rgb(255, 205, 86)", "#ffffff" ],
-            hoverOffset: 5
+            borderWidth: 1,
+            backgroundColor: [ "#fec42f", "#ffffff" ]
         };
         if ($jsChart3) new Chart($jsChart3, {
             type: "doughnut",
             data: {
                 datasets: [ jsChart3Y ]
             },
-            options: {
-                scales: {
-                    yAxes: [ {
-                        ticks: {
-                            beginAtZero: true
-                        }
-                    } ]
-                }
-            }
+            options: {}
         });
         const $box = document.querySelector("#box");
         const boxX = [ "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15" ];
